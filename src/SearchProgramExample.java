68b2cd33a15cc567feba16d70000105306a4ce83
Kyle <Kyle@DESKTOP-ILGFE70>
2017-02-02 19:43:57 -0500

    Worked in it a bit

--- src/SearchProgramExample.java
+++ src/SearchProgramExample.java
@@ -8,11 +8,13 @@ public class SearchProgramExample {
 	 * also the thing you print is a SearchNode, which may not tell you much.
 	 */
 	public static void main(String[] args) {
-		State initialState = null;
-		State goalState = null;
+		
+		String[] B1 = {"CAT", "DUCK", "GRAIN"};
+		String[] B2 = {null, null, null};
+		State initialState = new StateFarmer(B1, B2);
+		State goalState =  new StateFarmer(B2, B1);
 		StateSearcher toSearch = new StateSearcher(initialState, goalState);
 		SearchNode answer = toSearch.findOneSolution();
 		System.out.println(answer);
 	}
 }
-
--- src/StateFarmer.java
+++ src/StateFarmer.java
@@ -1,7 +1,36 @@
 import java.util.List;
 
-public class StateClass implements State {
+public class StateFarmer implements State {
 	
+	private String[] _bank1 = new String[3];
+	private String[] _bank2 = new String[3];
+	
+	public StateFarmer(String[] a, String[] b)
+	{
+		setBank1(a);
+		setBank2(b);
+	}
+	
+	public String[] getBank1() 
+	{
+		return _bank1;
+	}
+
+	public void setBank1(String _bank1[]) 
+	{
+		this._bank1 = _bank1;
+	}
+
+	public String[] getBank2() 
+	{
+		return _bank2;
+	}
+
+	public void setBank2(String _bank2[]) 
+	{
+		this._bank2 = _bank2;
+	}
+
 	@Override
 	public boolean equals(Object s)
 	{
